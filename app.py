243fec831802a85801088def54899dab6bd59905
Pedro Correia <p02.correia@gmail.com>
2025-11-27 10:16:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ with Home:
         if LOGO_PATH.exists():
             st.image(str(LOGO_PATH), width=130)
     with c2:
-        st.markdown("<h1 style=\"margin:.1rem 0 .6rem 0\"> Dockyard Acoustics Sound Technology </h2>")
+        st.markdown("## Dockyard Acoustics Sound Technology")
         st.markdown("<h2 style=\"margin:.1rem 0 .6rem 0\">State of the art Acoustic Simulation Software</h2>", unsafe_allow_html=True)
         st.write("Dockyard Acoustics is redefining how enthusiasts and professionals shape sound. Our Diffraction-Physics Room Acoustics Engine uses advanced, next-generation physics to deliver acoustic simulations with unprecedented speed, accuracy, and reliability. A all-in-one highly intuitive and portable platform.")
         st.write("We empower architects, acoustic consultants, and engineers to design and optimize acoustic environments with confidence. From early-stage concepts to final design refinement.")
